75660f32b69a25fa7df84c0658f587000d22dd28
lasvictorias <38115235+lasvictorias@users.noreply.github.com>
2018-09-09 22:32:11 -0700

    New probability distribution
    updated dimensions of mean (ft3/s) and included more statistical moments
    Note~ we get an infeasible problem when we constrain the kurtosis, so it's not currently used

--- MATLAB_Code/getProbDist.m
+++ MATLAB_Code/getProbDist.m
@@ -1,11 +1,19 @@
-% Script to generate probability distribution of surface runoff into first pond
+%% Script to generate probability distribution of surface runoff into first pond
 % Pr{wk = ws(i)} = P(i)
 % Fix possible values of wk & expected value of wk to generate P
 
-ws = 38000:1000:48000; %ft^3, possible values of wk
+%% Sample mean and moments
+mean = 12.16;
+variance = 3.22;
+skewness = 1.68;
+kurtosis = 8.31;
+
+%range of 
+ws = mean-2*sqrt(variance):0.5*sqrt(variance):mean+2.5*sqrt(variance); %ft^3/s, possible values of wk
 
 nw = length(ws);
 
+%% calculation feasible probability distribution
 cvx_begin
 
 variables P(nw,1)
@@ -14,7 +22,10 @@ variables P(nw,1)
 
     subject to
     
-        ws*P == 45000; % expected value of wk
+        ws*P == mean; % expected value of ws
+        (ws-mean).^2*P == variance; %variance of ws
+        (ws-mean).^3*P == skewness*variance^(3/2); %skewness of ws 
+        %(ws-mean).^4*P == kurtosis*variance^2; %kurtosis of ws
         
         P>=0;
         
